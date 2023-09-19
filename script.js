88566d67ac095626748ce1631ea18e56fe883fb0
Maillet Thibault <thibault.maillet@etu.univ-poitiers.fr>
2023-09-19 17:46:08 +0200

    different de second titre
    🎨 Améliore la gestion du titre

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("aa")
+console.log("Hello world")
+
+changerTitre();
+
+
+/**
+ * Changer le titre d'une page web
+ */
+function changerTitre() {
+    const title = document.getElementById('title');
+    document.getElementById('title').innerText += ' from JS';
+    document.getElementById('title').style.color = 'red';
+}
+
