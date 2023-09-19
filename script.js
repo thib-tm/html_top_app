cba08228640e12ec15248aadb4c5161ae36f18ef
Maillet Thibault <thibault.maillet@etu.univ-poitiers.fr>
2023-09-19 18:10:26 +0200

    🚧 ajout de l'heure

--- script.js
+++ script.js
@@ -16,3 +16,13 @@ const h2 = document.createElement("h2");
 h2.id = "sous-titre";
 h2.innerText = "Welcome to the javascript world";
 document.querySelector("h1").after(h2);
+
+// Add a h3 element with the current time just after the h2
+const h3 = document.createElement("h3");
+h3.innerText = new Date().toLocaleTimeString();
+document.querySelector("#sous-titre").after(h3);
+
+// Change the time every second
+setInterval(() => {
+    document.querySelector("h3").innerText = new Date().toLocaleTimeString();
+}, 1000);
